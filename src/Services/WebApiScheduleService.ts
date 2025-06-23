0ac98e9f96324a96d66a14b37ddc402a0276606b
Anthony Chambers <antmx1@gmail.com>
2025-06-23 23:19:55 +0100

    Added comment

--- src/Services/WebApiScheduleService.ts
+++ src/Services/WebApiScheduleService.ts
@@ -3,6 +3,10 @@ import Schedule from "../DataTypes/Schedule";
 import type IScheduleService from "./IScheduleService";
 import WebApiSvcBase from "./WebApiSvcBase";
 
+/**
+ * Concrete implementation of the IScheduleService interface.
+ * This service is responsible for fetching schedule data from the web API.
+ */
 export default class WebApiScheduleService extends WebApiSvcBase implements IScheduleService {
 
     async fetchAll(): Promise<Schedule[]> {
